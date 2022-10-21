173c7945d5eda978f5b661bac4d2c56ac8d19d1a
alexander.eder <alexander.eder@racing.tuwien.ac.at>
2022-10-21 15:32:23 +0200

    Put inputs in own file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import Colourbox from "./Colourbox";
+import Inputs from "./Inputs";
 
 function App() {
   return (
     <div className="container">
       <h1>GUESS THE COLOR</h1>
       <Colourbox />
-      <div className="name-input">
-        <input type="text" placeholder="Vorname" />
-        <input type="text" placeholder="Nachname" />
-      </div>
-      <div className="color-input">
-        <div className="text-color-input">
-          <input
-            type="text"
-            placeholder="red"
-            style={{ backgroundColor: "rgba(255,0,0,0.3)" }}
-          />
-          <input
-            type="text"
-            placeholder="green"
-            style={{ backgroundColor: "rgba(0,255,0,0.3)" }}
-          />
-          <input
-            type="text"
-            placeholder="blue"
-            style={{ backgroundColor: "rgba(0,0,255,0.3)" }}
-          />
-        </div>
-        <div className="slider-color-input">
-          <input type="range" style={{ trackcolor: "red", color: "green" }} />
-          <input type="range" />
-          <input type="range" />
-        </div>
-      </div>
+      <Inputs />
     </div>
   );
 }
---
+++ src/Inputs.jsx
@@ -0,0 +1,36 @@
+const Inputs = () => {
+  return (
+    <div className="inputs">
+      <div className="name-input">
+        <input type="text" placeholder="Vorname" />
+        <input type="text" placeholder="Nachname" />
+      </div>
+      <div className="color-input">
+        <div className="text-color-input">
+          <input
+            type="text"
+            placeholder="red"
+            style={{ backgroundColor: "rgba(255,0,0,0.3)" }}
+          />
+          <input
+            type="text"
+            placeholder="green"
+            style={{ backgroundColor: "rgba(0,255,0,0.3)" }}
+          />
+          <input
+            type="text"
+            placeholder="blue"
+            style={{ backgroundColor: "rgba(0,0,255,0.3)" }}
+          />
+        </div>
+        <div className="slider-color-input">
+          <input type="range" style={{ trackcolor: "red", color: "green" }} />
+          <input type="range" />
+          <input type="range" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Inputs;
